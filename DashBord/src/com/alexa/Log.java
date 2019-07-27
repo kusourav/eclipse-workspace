0b3ca0fcf4b5d5ac9f060caf9fb4c81f923be9d0
Sourav <souravconvex@gmail.com>
2019-07-27 22:26:31 +0530

    Initial commit

---
+++ DashBord/src/com/alexa/Log.java
@@ -0,0 +1,72 @@
+package com.alexa;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/Log")
+public class Log extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		Connection con = null;
+		PreparedStatement pstm = null;
+		ResultSet rs = null;
+	
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		
+		try {
+			
+			Class.forName("org.postgresql.Driver");
+			con = DriverManager.getConnection("jdbc:postgresql://Localhost:5432/AlexaDB", "postgres", "abcd1234"); 
+			
+			String query = "Select username, password from alexa.login_credentials where username=? and password=?";
+			
+			pstm = con.prepareStatement(query);
+			
+			pstm.setString(1, username);
+			pstm.setString(2, password);
+			
+			rs = pstm.executeQuery();
+			
+			if(rs.next())
+			{
+				RequestDispatcher rd = request.getRequestDispatcher("Main.jsp");
+				rd.include(request, response);
+			}
+			else
+			{
+				RequestDispatcher rd = request.getRequestDispatcher("Login.jsp");
+				rd.include(request, response);
+			}
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+		finally {
+			try {
+				
+				pstm.close();
+				con.close();
+				
+			} catch (Exception e) {
+				// TODO: handle exception
+				e.printStackTrace();
+			}
+		}
+		 
+	}
+
+}
